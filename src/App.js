97e85daf6c7d927f43ab2e852f301ffa6672db72
rahul <sandeepnagapeteru121@gmail.com>
2024-09-10 11:25:02 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+
+import {Route, Switch} from 'react-router-dom'
+import Register from './components/Register'
+import Home from './components/Home'
+
+import NotFound from './components/NotFound'
+
+import RegisterContext from './components/context/RegisterContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    name: '',
+    topic: 'Arts and Culture',
+    isRegistered: false,
+    showError: false,
+  }
+
+  changeName = name => {
+    this.setState({name})
+  }
+
+  changeTopic = topic => {
+    this.setState({topic})
+  }
+
+  registerName = () => {
+    this.setState({isRegistered: true})
+  }
+
+  updateError = () => {
+    this.setState({showError: true})
+  }
+
+  render() {
+    const {name, topic, isRegistered, showError} = this.state
+    return (
+      <RegisterContext.Provider
+        value={{
+          name,
+          topic,
+          isRegistered,
+          showError,
+          changeName: this.changeName,
+          changeTopic: this.changeTopic,
+          registerName: this.registerName,
+          updateError: this.updateError,
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <NotFound />
+        </Switch>
+      </RegisterContext.Provider>
+    )
+  }
+}
+
+export default App
